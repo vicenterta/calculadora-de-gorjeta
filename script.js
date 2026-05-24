8fdde3a87d533fcfc9f0a57d9138b03fb147022e
vicenterta <maria.rvicente@eaportal.org>
2026-05-23 22:04:44 -0500

    sexto commit do JS e AGORA SIM PROJETO FINALIZADO

--- script.js
+++ script.js
@@ -80,4 +80,8 @@ function limpar () {
 
     document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
     document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+
+    conta = 0
+    pessoas = 0
+    porcentagem = 0
 }
